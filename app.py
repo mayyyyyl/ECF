cbd3e2705b5953dc509079e0eec8cc2328bbd2eb
Maylis Le Borgne <maylislb@hotmail.fr>
2022-04-19 19:16:23 +0200

    modification d'affichage

--- app.py
+++ app.py
@@ -142,12 +142,12 @@ def init_db():
         password = click.prompt('Your password', type=str, hide_input=True)
         email = click.prompt('Your email address', type=str)
 
-        newhotel = Hotel.create(name='Mon premier Hotel', address='9 Rue de la Fontaine Grillée', city='La Haie-Fouassière', description='ipsum in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque')
+        newhotel = Hotel.create(name='Four Seasons', address='2 boulevard de la Liberté', city='Paris', description='ipsum in blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in sapien iaculis congue vivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque')
         Suite.create(titre='Suite de reve', img='Jules-Perline.jpg', description='description de la suite', price='300', link='https://www.booking.com/hotel/fr/holiday-home-bucolique.fr.html?aid=390156;label=duc511jc-1DCAsoTUIWaG9saWRheS1ob21lLWJ1Y29saXF1ZUgzWANoTYgBAZgBDbgBF8gBDNgBA-gBAYgCAagCA7gC3OLIkQbAAgHSAiQ1NTI1NmFkOC00Y2MzLTQ4MjAtYmNlNC1hM2RiYzFkOTJkM2LYAgTgAgE;sid=2b3a5887ae5f0c86c2fcc89e7a12a735;dist=0&keep_landing=1&sb_price_type=total&type=total&', hotel=newhotel.id)
 
         User.create(firstname=first, lastname=last, email=email, password=hashingpassword(password), is_admin=True)
-        User.create(firstname='paul', lastname='Dupont', email='paul@email.fr', password=hashingpassword('paul'))
-        newgerant = User.create(firstname='jean', lastname='Dupont', email='jean@email.fr', password=hashingpassword('jean'))
+        User.create(firstname='Paul', lastname='Dupont', email='paul@email.fr', password=hashingpassword('paul'))
+        newgerant = User.create(firstname='Jean', lastname='Dupont', email='jean@email.fr', password=hashingpassword('jean'))
         Gerant.create(user=newgerant.id, hotel=newhotel.id)
 
     except Exception:
